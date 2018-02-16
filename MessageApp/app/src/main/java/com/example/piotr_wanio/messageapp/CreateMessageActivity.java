9e0ef294e755b9ed50da1e311aecc78aadd308b5
piotrwanio <34741162+piotrwanio@users.noreply.github.com>
2018-02-16 11:45:27 +0100

    added new project created in Android Studio + added new activity

---
+++ MessageApp/app/src/main/java/com/example/piotr_wanio/messageapp/CreateMessageActivity.java
@@ -0,0 +1,20 @@
+package com.example.piotr_wanio.messageapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        startActivity(intent);
+    }
+}
